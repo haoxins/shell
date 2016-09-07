ac276569430758c4c6137a69b81da0e2f28611ec
haoxin <coderhaoxin@outlook.com>
2016-09-07 09:37:29 +0800

    add config

---
+++ config/gitignore.md
@@ -0,0 +1,11 @@
+# js
+
+npm-debug.log
+node_modules/
+coverage/
+
+# py
+
+__pycache__/
+.DS_Store
+*.py[cod]
---
+++ config/vscode.js
@@ -0,0 +1,12 @@
+// Place your settings in this file to overwrite the default settings
+{
+  "editor.fontSize": 16,
+  "editor.renderWhitespace": true,
+  "editor.tabSize": 2,
+  "editor.scrollBeyondLastLine": false,
+  "diffEditor.ignoreTrimWhitespace": false,
+  "git.enabled": true,
+  "files.trimTrailingWhitespace": true,
+  "javascript.validate.enable": false,
+  "css.validate": false
+}
