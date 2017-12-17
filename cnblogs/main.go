ee6634d1def494731b36af3692585b02f325d522
haoxin <haoxins@outlook.com>
2017-12-17 22:48:23 +0800

    cnblogs - read article

--- cnblogs/main.go
+++ cnblogs/main.go
@@ -3,8 +3,26 @@ package main
 import "github.com/PuerkitoBio/goquery"
 import "strings"
 import "fmt"
+import "os"
 
 func main() {
+
+	if len(os.Args) > 1 {
+		id := os.Args[1]
+		doc, err := goquery.NewDocument("https://news.cnblogs.com/n/" + id)
+		if err != nil {
+			fmt.Printf("query error: %v", err)
+		}
+
+		doc.Find("#news_content #news_body p").Each(func(i int, s *goquery.Selection) {
+			p := s.Text()
+
+			fmt.Printf("%s \n", p)
+		})
+
+		return
+	}
+
 	doc, err := goquery.NewDocument("https://news.cnblogs.com")
 	if err != nil {
 		fmt.Printf("query error: %v", err)
