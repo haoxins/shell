5d2db6a584b706819cfadf7b545a25a3d2772021
haoxin <haoxins@icloud.com>
2018-04-29 15:32:22 +0800

    update iex

--- iex/config.go
+++ iex/config.go
@@ -3,7 +3,10 @@ package main
 import "github.com/parnurzeal/gorequest"
 
 const host = "https://api.iextrading.com/1.0"
+const zhHost = "https://hq.sinajs.cn"
 
 var request = gorequest.New()
 
 var cList = []string{"nvda", "baba", "jd"}
+
+var zhList = []string{"sz000725"}
